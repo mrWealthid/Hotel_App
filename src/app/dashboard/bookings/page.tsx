078aeff16b6336fd328b82a794b69b60f53366ed
wealthiduwe <wealthiduwe@gmail.com>
2023-10-04 11:41:56 -0400

    Update: Updated cabin and booings page

--- src/app/dashboard/bookings/page.tsx
+++ src/app/dashboard/bookings/page.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BookingList from './list/BookingList';
 
-const page = () => {
-	return <div> Bookings page</div>;
+import AddBooking from './AddBooking';
+
+const Page = () => {
+	return (
+		<div className="w-full">
+			<div className="flex items-center justify-between">
+				<h1 className="title"> All Bookings </h1>
+
+				<AddBooking />
+			</div>
+
+			{/* <CabinForm /> */}
+			<BookingList />
+		</div>
+	);
 };
 
-export default page;
+export default Page;
--- src/app/dashboard/cabins/AddCabin.tsx
+++ src/app/dashboard/cabins/AddCabin.tsx
@@ -1,7 +1,11 @@
 'use client';
 import ButtonComponent from '@/components/shared/Button-component';
 import ModalComponent from '@/components/shared/Modal-component';
+import Cabin from '@/model/cabinModel';
 import React, { useState } from 'react';
+import CabinForm from './CabinForm';
+import Modal from '@/components/shared/Modal-component';
+import CabinList from './list/CabinList';
 
 const AddCabin = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -10,17 +14,30 @@ const AddCabin = () => {
 		setIsOpen((prev) => !prev);
 	}
 	return (
-		<div>
-			<div className="flex justify-end ">
-				<ButtonComponent
-					type="button"
-					style="rounded-3xl  "
-					btnText={'Add New Cabin'}
-					handleClick={handleToggle}></ButtonComponent>
-			</div>
+		<Modal>
+			<Modal.Open opens="cabin-form">
+				<div>
+					<button type="button" className="btn-primary rounded-3xl">
+						Add New Cabin
+					</button>
+				</div>
+			</Modal.Open>
+			<Modal.Window name="cabin-form">
+				<CabinForm />
+			</Modal.Window>
+		</Modal>
 
-			{isOpen && <ModalComponent onClose={() => setIsOpen(false)} />}
-		</div>
+		// <div>
+		// 	<div className="flex justify-end ">
+		// 		<ButtonComponent
+		// 			type="button"
+		// 			style="rounded-3xl  "
+		// 			btnText={'Add New Cabin'}
+		// 			handleClick={handleToggle}></ButtonComponent>
+		// 	</div>
+
+		// 	{isOpen && <ModalComponent onClose={() => setIsOpen(false)} />}
+		// </div>
 	);
 };
 
