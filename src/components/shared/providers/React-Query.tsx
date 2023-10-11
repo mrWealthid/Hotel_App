e5b0590643ff88ae8293446889bbc8a292acdc29
wealthiduwe <wealthiduwe@gmail.com>
2023-10-11 18:20:07 -0400

    add: Added a provider for react query

---
+++ src/components/shared/providers/React-Query.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { ReactNode } from 'react';
+
+export default function Providers({ children }: any) {
+	const [queryClient] = React.useState(() => new QueryClient());
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+		</QueryClientProvider>
+	);
+}
