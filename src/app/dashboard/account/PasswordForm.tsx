da150775db64da41a34cabea4a1ad5a570134249
wealthiduwe <wealthiduwe@gmail.com>
2023-10-20 21:44:22 -0400

    add: Added account module

---
+++ src/app/dashboard/account/PasswordForm.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import TextInput from '@/components/shared/Form-inputs/Text-Input';
+import ButtonComponent from '@/components/shared/Form-inputs/Button-component';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import EmailInput from '@/components/shared/Form-inputs/Email-Input';
+import { useProfile } from '../profile/hooks/useProfile';
+
+const PasswordForm = ({ stage, updateStage }: any) => {
+	const { data, isLoading } = useProfile();
+	// const isEditing = !!cabin?.id;
+
+	// const isEditing = true;
+
+	// console.log(data);
+	// console.log(isLoading);
+
+	const { register, handleSubmit, getValues, formState } = useForm({
+		mode: 'onChange'
+
+		// values: data
+	});
+
+	const { errors, isSubmitting } = formState;
+
+	async function onSubmit(data: any) {
+		console.log(data);
+		stage === 0 && updateStage(stage + 1);
+		// try {
+		// 	const res = await fetch(`http://localhost:3000/api/user`, {
+		// 		method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
+		// 		body: JSON.stringify(data) // body data type must match "Content-Type" header
+		// 	});
+
+		// 	if (!res.ok) {
+		// 		throw new Error(
+		// 			`Cabin could not be created Status: ${res.status}`
+		// 		);
+		// 	}
+		// 	stage === 0 && updateStage(stage + 1);
+		// 	return res.json(); // parses JSON response into native JavaScript objects
+		// } catch (err) {
+		// 	console.log(err);
+		// }
+	}
+
+	function onError(err: any) {
+		console.log(err);
+	}
+
+	return (
+		<form
+			onSubmit={handleSubmit(onSubmit, onError)}
+			className="flex flex-col gap-3">
+			<TextInput
+				name={'password'}
+				placeholder="Enter Password"
+				label="Password"
+				type="password">
+				<input
+					{...register('password', {
+						required: 'This field is required'
+					})}
+					className="input-style"
+					type="password"
+					id="password"
+				/>
+			</TextInput>
+			<TextInput
+				name={'confirmPassword'}
+				placeholder="Enter Password"
+				label="Confirm Password">
+				<input
+					{...register('confirmPassword', {
+						required: 'This field is required'
+					})}
+					className="input-style"
+					type="password"
+					id="confirmPassword"
+				/>
+			</TextInput>
+
+			<div className="mt-2 gap-3 flex justify-end ">
+				<ButtonComponent
+					type="reset"
+					handleClick={() => updateStage(0)}
+					style="rounded-3xl"
+					btnText={'Cancel'}></ButtonComponent>
+
+				<ButtonComponent
+					type="submit"
+					style="rounded-3xl"
+					disabled={!formState.isValid}
+					btnText={'Update Profile'}></ButtonComponent>
+			</div>
+		</form>
+	);
+};
+
+export default PasswordForm;
---
+++ src/app/dashboard/account/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import React, { useState } from 'react';
+import AccountForm from './accountForm';
+import { getData } from '@/utils/apiRequests';
+import { useProfile } from '../profile/hooks/useProfile';
+import Image from 'next/image';
+
+const Page = () => {
+	const [stage, setStage] = useState<number>(0);
+	const { data, isLoading } = useProfile();
+
+	function updateStage(val: number) {
+		setStage(val);
+	}
+	return (
+		<section className="">
+			<h1 className="title">Account </h1>
+			<section className="flex dark:text-secondary text-primary lg:flex-row flex-col w-full animate-slideIn container-text  gap-3 ">
+				<section className=" skin  dark:glass h-fit p-6  flex gap-6 border dark:border-none  w-full  lg:w-1/2 2xl:w-1/3 rounded-2xl">
+					<div className="py-3">
+						<Image
+							className="border dark:border-primary-light rounded-full mx-0 w-32 object-cover h-32"
+							height="820"
+							alt="default"
+							src={'/images/default.jpg'}
+							width="900"
+						/>
+
+						{/* <Image
+						width={25}
+						height={25}
+						className="border rounded-full"
+						alt="default"
+						src={'/images/default.jpg'}
+					/> */}
+					</div>
+					<section className="flex flex-col gap-4">
+						<p className="lg:text-lg">Personal Details</p>
+						<span className="text-xs">
+							<i className="fa-solid fa-user"></i> {data?.name}
+						</span>
+						<span className="text-xs">
+							<i className="fa-solid  fa-at"></i>
+							{data?.email}
+						</span>
+
+						<span className=" flex capitalize justify-center w-1/2 px-2 py-1 text-sm rounded-full">
+							{data?.role}
+						</span>
+					</section>
+				</section>
+
+				<AccountForm
+					data={data}
+					stage={stage}
+					updateStage={updateStage}
+				/>
+			</section>
+		</section>
+	);
+};
+
+export default Page;
