91f54b81a979d75e81b62e7f118ed114b559b237
wealthiduwe <wealthiduwe@gmail.com>
2023-10-22 21:34:05 -0400

    add: Added dashboard analytic page & logic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import { useRecentBookings, useRecentStays } from './hooks/useDashboard';
+import StatsComponent from '@/components/ui/StatsComponent';
 
-const page = () => {
-	return <div>My Dashboard Page</div>;
+const Page = () => {
+	const [days, setDays] = useState<number>(7);
+
+	// console.log(query);
+	async function handleClick(query: any) {
+		setDays(query);
+		// handleFilter(query);
+	}
+
+	const { bookingsError, bookingsLoading, bookings } =
+		useRecentBookings(days);
+	const { staysLoading, staysError, stays } = useRecentStays(days);
+
+	// function handleFilter() {
+	// 	// setfilterIsActive(false);
+	// 	// setSearch(null);
+	// 	return;
+	// }
+
+	return (
+		<div>
+			My Dashboard Page
+			<section className=" flex gap-2 justify-end">
+				<div className="">
+					<button
+						onClick={() => handleClick(7)}
+						type="button"
+						className={`${
+							days === 7 && 'bg-primary text-white'
+						} w-full  text-xs px-6 py-2 rounded-3xl  bg-gray-50 font-light text-black border btn`}>
+						Last 7 days
+					</button>
+				</div>
+
+				<div className="">
+					<button
+						onClick={() => handleClick(30)}
+						type="button"
+						className={`${
+							days === 30 && 'bg-primary text-white'
+						} w-full  text-xs px-6 py-2 rounded-3xl  bg-gray-50 font-light text-black border btn`}>
+						Last 30 days
+					</button>
+				</div>
+				<div className="">
+					<button
+						onClick={() => handleClick(90)}
+						type="button"
+						className={`${
+							days === 90 && 'bg-primary text-white'
+						} w-full  text-xs px-6 py-2 rounded-3xl  bg-gray-50 font-light text-black border btn`}>
+						{' '}
+						Last 90 days
+					</button>
+				</div>
+			</section>
+			<StatsComponent bookings={bookings} stays={stays} />
+		</div>
+	);
 };
 
-export default page;
+export default Page;
