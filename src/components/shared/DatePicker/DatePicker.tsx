b0f6f427c6b6edf0545f43857fd90219ad7bbac5
wealthiduwe <wealthiduwe@gmail.com>
2023-11-07 15:59:55 -0500

    Added: Reusable date range for bookings

---
+++ src/components/shared/DatePicker/DatePicker.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import Label from '../Form-inputs/Label';
+import { addDays } from 'date-fns';
+
+export const DateRangePicker = ({
+	startDate,
+	endDate,
+	handleStartDate,
+	handleEndDate,
+	labelText,
+	maxDate = null,
+	minDate = startDate
+}: any) => {
+	// const [startDate, setStartDate] = useState(null);
+	// const [endDate, setEndDate] = useState(null);
+
+	return (
+		<>
+			<Label name={labelText} text={labelText} />
+			<section className="flex -mt-2 gap-2 w-full">
+				<DatePicker
+					selected={startDate}
+					onChange={(date: any) => {
+						handleStartDate(date);
+						handleEndDate(null); //This will reset end date when state date changes
+					}}
+					selectsStart
+					startDate={startDate}
+					endDate={endDate}
+					isClearable={true}
+					placeholderText="Start Date"
+					className="input-style"
+                    required
+				/>
+				<DatePicker
+					selected={endDate}
+					onChange={(date: any) => handleEndDate(date)}
+					selectsEnd
+					startDate={startDate}
+					endDate={endDate}
+					minDate={minDate}
+					isClearable={true}
+					maxDate={maxDate}
+					className="input-style"
+					placeholderText="End Date"
+                    required
+				/>
+			</section>
+		</>
+	);
+};
