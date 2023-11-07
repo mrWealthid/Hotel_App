05d4794dae79959e71e4b494b4f3a3f6415f77a7
wealthiduwe <wealthiduwe@gmail.com>
2023-11-07 16:00:23 -0500

    Add: Added autocomplete and date range selectors

--- src/app/dashboard/bookings/BookingForm.tsx
+++ src/app/dashboard/bookings/BookingForm.tsx
@@ -2,23 +2,37 @@
 
 import TextInput from '@/components/shared/Form-inputs/Text-Input';
 import ButtonComponent from '@/components/shared/Form-inputs/Button';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import AutoComplete from '@/components/shared/AutoComplete/AutoComplete';
 import { fetchCabins, fetchGuests } from './service/bookings.service';
+import { DateRangePicker } from '@/components/shared/DatePicker/DatePicker';
+import { addDays, differenceInDays, formatISO } from 'date-fns';
+import { formatCurrency } from '@/utils/helpers';
 
 const BookingForm = ({ booking, onCloseModal, settings }: any) => {
 	const isEditing = !!booking?.id;
-
-	const { register, handleSubmit, getValues, formState } = useForm({
-		mode: 'onChange',
-		defaultValues: isEditing ? { ...booking } : {}
-	});
-
 	const [autoCompleteValue, setAutoCompleteValue] = useState<{
 		guests: any;
 		cabin: any;
 	} | null>(null);
+
+	const [startDate, setStartDate] = useState<any>(null);
+	const [endDate, setEndDate] = useState<any>(null);
+
+	const [breakfastPrice, setBreakfastPrice] = useState<any>(0);
+	const { register, handleSubmit, getValues, formState } = useForm({
+		mode: 'all',
+		defaultValues: isEditing ? { ...booking } : {},
+		values: {
+			cabin: autoCompleteValue?.cabin?.id,
+			guests: autoCompleteValue?.guests?.id,
+			startDate: startDate,
+			endDate: endDate
+		}
+	});
+
+	const [hasBreakfast, setHasBreakFast] = useState(false);
 
 	const { errors, isSubmitting } = formState;
 
@@ -26,17 +40,43 @@ const BookingForm = ({ booking, onCloseModal, settings }: any) => {
 		setAutoCompleteValue({ ...autoCompleteValue, ...values });
 	}
 
+	function handleStartDate(date: any) {
+		setStartDate(date);
+	}
+	function handleEndDate(date: any) {
+		setEndDate(date);
+	}
+
+	useEffect(() => {
+		setBreakfastPrice(calculateBreakfastPrice());
+	}, [formState.isValid]);
+
 	async function onSubmit(data: any) {
 		const { guests, cabin }: any = autoCompleteValue;
+		const diffInDays = differenceInDays(
+			new Date(endDate),
+			new Date(startDate)
+		);
 
 		const payload = {
 			...data,
-			guests: guests.id,
-			cabin: cabin.id,
-			regularPrice: cabin.regularPrice
+			discount: cabin.discount,
+			cabinPrice: cabin.regularPrice,
+			startDate: formatISO(startDate),
+			endDate: formatISO(endDate),
+			numNights: diffInDays
 		};
 
-		console.log(payload);
+		console.log(data);
+
+		console.log('PAYLOAD==>', payload);
+
+		if (hasBreakfast) payload.hasBreakfast = hasBreakfast;
+		payload.extraPrice =
+			settings.breakfastPrice *
+			diffInDays *
+			parseInt(getValues().numGuests);
+
 		try {
 			const res = await fetch(
 				`${
@@ -64,20 +104,63 @@ const BookingForm = ({ booking, onCloseModal, settings }: any) => {
 	function onError(err: any) {
 		console.log(err);
 	}
+
+	function addBreakfast(e: any) {
+		setHasBreakFast(e.target.checked);
+	}
+
+	function calculateBreakfastPrice() {
+		const diffInDays = differenceInDays(
+			new Date(endDate),
+			new Date(startDate)
+		);
+
+		console.log(diffInDays);
+		console.log(getValues().numGuests);
+		console.log(settings.breakfastPrice);
+
+		// console.log(
+		// 	parseInt(settings.breakfastPrice) *
+		// 		diffInDays *
+		// 		parseInt(getValues().numGuests)
+		// );
+		return (
+			parseInt(settings.breakfastPrice) *
+			diffInDays *
+			parseInt(getValues().numGuests)
+		);
+	}
 	return (
 		<div className="w-full">
-			{/* <div className="flex justify-end ">
-				<ButtonComponent
-					type="button"
-					style="rounded-3xl  "
-					btnText={'Add New Cabin'}
-					handleClick={handleToggle}></ButtonComponent>
-			</div> */}
-
 			<form
 				onSubmit={handleSubmit(onSubmit, onError)}
 				className=" flex flex-1  p-6 bg-white   items-center">
 				<section className="flex-col flex gap-2 w-full">
+					<AutoComplete
+						queryKey="guests"
+						service={fetchGuests}
+						label={'Guest'}
+						displayValue={'name'}
+						handler={handleAutoCompleteValues}
+					/>
+
+					<div className="hidden">
+						<TextInput
+							name={'guests'}
+							error={errors?.['guests']?.message?.toString()}>
+							<input
+								title="cabin"
+								{...register('guests', {
+									required: 'This field is required'
+								})}
+								className="input-style"
+								type="text"
+								hidden
+								readOnly
+								id="guests"
+							/>
+						</TextInput>
+					</div>
 					<TextInput
 						name={'numGuests'}
 						placeholder="Enter Number Of Guests"
@@ -103,14 +186,6 @@ const BookingForm = ({ booking, onCloseModal, settings }: any) => {
 							id="numGuests"
 						/>
 					</TextInput>
-
-					<AutoComplete
-						queryKey="guests"
-						service={fetchGuests}
-						label={'Guest'}
-						displayValue={'name'}
-						handler={handleAutoCompleteValues}
-					/>
 					<AutoComplete
 						queryKey="cabin"
 						service={fetchCabins}
@@ -118,25 +193,65 @@ const BookingForm = ({ booking, onCloseModal, settings }: any) => {
 						displayValue={'name'}
 						handler={handleAutoCompleteValues}
 					/>
+					<div className="hidden">
+						<TextInput
+							name={'cabin'}
+							error={errors?.['cabin']?.message?.toString()}>
+							<input
+								title="cabin"
+								{...register('cabin', {
+									required: 'This field is required'
+								})}
+								className="input-style"
+								type="text"
+								hidden
+								readOnly
+								id="cabin"
+							/>
+						</TextInput>
+					</div>
 
-					<TextInput
-						name={'regularPrice'}
-						placeholder="Enter regularPrice"
-						label="Regular Price"
-						error={errors?.['regularPrice']?.message?.toString()}>
-						<input
-							title="regularPrice"
-							value={autoCompleteValue?.cabin?.regularPrice}
-							className="input-style"
-							type="text"
-							readOnly
-							disabled={isSubmitting}
-							id="regularPrice"
-						/>
-					</TextInput>
-
-					{/* <span>{autoCompleteValue?.cabin?.regularPrice}</span> */}
-					<TextInput
+					<section className="flex items-center gap-2">
+						<TextInput
+							name={'regularPrice'}
+							placeholder="Enter regularPrice"
+							label="Regular Price"
+							error={errors?.[
+								'regularPrice'
+							]?.message?.toString()}>
+							<input
+								title="regularPrice"
+								// {...register('regularPrice', {
+								// 	required: 'This field is required'
+								// })}
+								value={
+									autoCompleteValue?.cabin?.regularPrice || ''
+								}
+								readOnly
+								className="input-style"
+								type="text"
+								id="regularPrice"
+							/>
+						</TextInput>
+						<TextInput
+							name={'discount'}
+							placeholder="Enter discount"
+							label="Discount"
+							error={errors?.['discount']?.message?.toString()}>
+							<input
+								title="discount"
+								// {...register('discount', {
+								// 	required: 'This field is required'
+								// })}
+								value={autoCompleteValue?.cabin?.discount || ''}
+								className="input-style"
+								type="number"
+								readOnly
+								id="discount"
+							/>
+						</TextInput>
+					</section>
+					{/* <TextInput
 						name={'discount'}
 						placeholder="Enter Discount"
 						label="Discount"
@@ -159,7 +274,7 @@ const BookingForm = ({ booking, onCloseModal, settings }: any) => {
 							id="discount"
 							defaultValue={0}
 						/>
-					</TextInput>
+					</TextInput> */}
 
 					<TextInput
 						name={'observation'}
@@ -177,8 +292,81 @@ const BookingForm = ({ booking, onCloseModal, settings }: any) => {
 							rows={3}></textarea>
 					</TextInput>
 
-					{/* <label htmlFor="photo">Photo</label>
-					<p>Image Upload</p> */}
+					<DateRangePicker
+						labelText={'Duration'}
+						startDate={startDate}
+						endDate={endDate}
+						handleStartDate={handleStartDate}
+						handleEndDate={handleEndDate}
+						maxDate={
+							startDate
+								? addDays(startDate, settings.maxBookingLength)
+								: null
+						}
+						minDate={
+							startDate
+								? addDays(startDate, settings.minBookingLength)
+								: null
+						}
+					/>
+
+					<div className="hidden">
+						<TextInput name={'startDate'}>
+							<input
+								title="startDate"
+								{...register('startDate', {
+									required: 'This field is required'
+								})}
+								className="input-style"
+								readOnly
+								disabled={isSubmitting}
+								id="startDate"
+							/>
+						</TextInput>
+					</div>
+					<div className="hidden">
+						<TextInput name={'endDate'}>
+							<input
+								title="endDate"
+								{...register('endDate', {
+									required: 'This field is required'
+								})}
+								className="input-style"
+								readOnly
+								disabled={isSubmitting}
+								id="endDate"
+							/>
+						</TextInput>
+					</div>
+
+					{formState.isValid && (
+						<section className="bg-white text-xs  items-center flex gap-2  mt-1">
+							<input
+								title="check"
+								id="checkbox-all-search"
+								type="checkbox"
+								checked={hasBreakfast}
+								onChange={addBreakfast}
+								className="w-4 h-4 m-0 border-gray-300 rounded focus:ring-gray-500 "
+							/>
+							<label
+								htmlFor="checkbox-all-search text-sm"
+								className="sr-only">
+								#
+							</label>
+
+							<span>
+								Want to add breakfast
+								<span>
+									{' '}
+									for{' '}
+									{formState.isValid &&
+										formatCurrency(breakfastPrice)}
+									?
+								</span>
+							</span>
+						</section>
+					)}
 
 					<section className="flex justify-end gap-4">
 						<ButtonComponent
