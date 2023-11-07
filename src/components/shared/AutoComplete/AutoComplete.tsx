f0e8c96582b9c5ff523fad3a992ab9fe12edb035
wealthiduwe <wealthiduwe@gmail.com>
2023-11-07 16:01:28 -0500

    Update: Updated autocomplete component

--- src/components/shared/AutoComplete/AutoComplete.tsx
+++ src/components/shared/AutoComplete/AutoComplete.tsx
@@ -32,18 +32,6 @@ export default function AutoComplete({
 		autoCompleteResult: data
 	} = useAutoComplete(query, service, queryKey);
 
-	console.log(data);
-
-	// const filteredPeople =
-	// 	query === ''
-	// 		? people
-	// 		: people.filter((result) =>
-	// 				result.name
-	// 					.toLowerCase()
-	// 					.replace(/\s+/g, '')
-	// 					.includes(query.toLowerCase().replace(/\s+/g, ''))
-	// 		  );
-
 	function handleChangeEvent(val: any) {
 		setSelected(val);
 		handler({ [queryKey]: val });
@@ -52,7 +40,7 @@ export default function AutoComplete({
 	return (
 		<div className=" w-full">
 			<Label name={''} text={label} />
-			<p>{selected?.id}</p>
+			{/* <p>{selected?.id}</p> */}
 			<Combobox
 				value={selected}
 				onChange={(selected) => handleChangeEvent(selected)}>
@@ -62,7 +50,6 @@ export default function AutoComplete({
 							className="w-full border-none z-10 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
 							displayValue={(result: any) => result[displayValue]}
 							onChange={(event) => setQuery(event.target.value)}
-							//
 						/>
 						<Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
 							{/* <SelectorIcon
@@ -126,52 +113,3 @@ export default function AutoComplete({
 		</div>
 	);
 }
-
-// <Transition
-// 	as={Fragment}
-// 	leave="transition ease-in duration-100"
-// 	leaveFrom="opacity-100"
-// 	leaveTo="opacity-0"
-// 	afterLeave={() => setQuery('')}>
-// 	<Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-// 		{filteredPeople.length === 0 && query !== '' ? (
-// 			<div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-// 				Nothing found.
-// 			</div>
-// 		) : (
-// 			filteredPeople.map((result) => (
-// 				<Combobox.Option
-// 					key={result.id}
-// 					className={({ active }) =>
-// 						`relative cursor-default select-none py-2 pl-10 pr-4 ${
-// 							active
-// 								? 'bg-teal-600 text-white'
-// 								: 'text-gray-900'
-// 						}`
-// 					}
-// 					value={result}>
-// 					{({ selected, active }) => (
-// 						<>
-// 							<span
-// 								className={`block truncate ${
-// 									selected ? 'font-medium' : 'font-normal'
-// 								}`}>
-// 								{result.name}
-// 							</span>
-// 							{selected ? (
-// 								<span
-// 									className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-// 										active
-// 											? 'text-white'
-// 											: 'text-teal-600'
-// 									}`}>
-// 									{/* <CheckIcon className="h-5 w-5" aria-hidden="true" /> */}
-// 								</span>
-// 							) : null}
-// 						</>
-// 					)}
-// 				</Combobox.Option>
-// 			))
-// 		)}
-// 	</Combobox.Options>
-// </Transition>;
