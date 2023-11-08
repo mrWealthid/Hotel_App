1f16446b7c4f35fec8c18f66ccf6b988d2ad1e22
wealthiduwe <wealthiduwe@gmail.com>
2023-11-07 22:34:42 -0500

    add: Added Icons for accessibilty

--- src/components/shared/AutoComplete/AutoComplete.tsx
+++ src/components/shared/AutoComplete/AutoComplete.tsx
@@ -4,16 +4,7 @@ import { Combobox, Transition } from '@headlessui/react';
 import { useAutoComplete } from './AutoCompleteHook';
 import Label from '../Form-inputs/Label';
 
-// import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
-
-// const people = [
-// 	{ id: 1, name: 'Wade Cooper' },
-// 	{ id: 2, name: 'Arlene Mccoy' },
-// 	{ id: 3, name: 'Devon Webb' },
-// 	{ id: 4, name: 'Tom Cook' },
-// 	{ id: 5, name: 'Tanya Fox' },
-// 	{ id: 6, name: 'Hellen Schmidt' }
-// ];
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
 
 export default function AutoComplete({
 	service,
@@ -47,15 +38,15 @@ export default function AutoComplete({
 				<div className="relative mt-1">
 					<div className="input-style !py-1">
 						<Combobox.Input
-							className="w-full border-none z-10 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+							className="w-full border-none  py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
 							displayValue={(result: any) => result[displayValue]}
 							onChange={(event) => setQuery(event.target.value)}
 						/>
 						<Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-							{/* <SelectorIcon
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              /> */}
+							<SelectorIcon
+								className="h-5 w-5 text-gray-400"
+								aria-hidden="true"
+							/>
 						</Combobox.Button>
 					</div>
 					<Transition
@@ -76,7 +67,7 @@ export default function AutoComplete({
 										className={({ active }) =>
 											`relative z-50 cursor-default select-none py-2 pl-10 pr-4 ${
 												active
-													? 'bg-teal-600 text-white'
+													? 'bg-primary text-white'
 													: 'text-gray-900'
 											}`
 										}
@@ -98,7 +89,10 @@ export default function AutoComplete({
 																? 'text-white'
 																: 'text-teal-600'
 														}`}>
-														{/* <CheckIcon className="h-5 w-5" aria-hidden="true" /> */}
+														<CheckIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
 													</span>
 												) : null}
 											</>
