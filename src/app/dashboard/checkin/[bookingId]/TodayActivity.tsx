502967e296a41968bd376f837959f2027a114da8
wealthiduwe <wealthiduwe@gmail.com>
2023-11-10 11:32:39 -0500

    Add:Added A reusable tab component

--- src/app/dashboard/checkin/[bookingId]/TodayActivity.tsx
+++ src/app/dashboard/checkin/[bookingId]/TodayActivity.tsx
@@ -3,6 +3,14 @@ import TodayItem from './TodayItem';
 
 const TodayActivity = ({ daily }: any) => {
 	console.log(daily);
+
+	if (daily?.length < 1) {
+		return (
+			<p className="  text-xs   dark:glass p-3 dark:rounded   items-center  ">
+				No Data Available
+			</p>
+		);
+	}
 	return (
 		<div className=" flex flex-col gap-2">
 			{daily?.map((activity: any) => (
