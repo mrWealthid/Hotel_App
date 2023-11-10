9e184a259f8ef6ef0f48c54c65d344ea1bd67fc5
wealthiduwe <wealthiduwe@gmail.com>
2023-11-10 11:35:29 -0500

    Bug Fix

--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -4,20 +4,18 @@ import React from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
-import { DarkModeProvider } from './LightDarkModeContext';
+// import { DarkModeProvider } from './LightDarkModeContext';
 
 function Providers({ children }: React.PropsWithChildren) {
 	const [client] = React.useState(new QueryClient());
 
 	return (
-		<DarkModeProvider>
-			<QueryClientProvider client={client}>
-				<ReactQueryStreamedHydration>
-					{children}
-				</ReactQueryStreamedHydration>
-				<ReactQueryDevtools initialIsOpen={false} />
-			</QueryClientProvider>
-		</DarkModeProvider>
+		<QueryClientProvider client={client}>
+			<ReactQueryStreamedHydration>
+				{children}
+			</ReactQueryStreamedHydration>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 }
 
