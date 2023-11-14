cda1a9d341ba9f4c35436d021f954587105604d5
wealthiduwe <wealthiduwe@gmail.com>
2023-11-13 21:15:38 -0500

    Fix: Converted dashboard stats to axios for better error handling

--- src/app/dashboard/service/dashboard.service.ts
+++ src/app/dashboard/service/dashboard.service.ts
@@ -4,30 +4,21 @@ export async function fetchBookingsAfterDate(days: number) {
 	const url = `/api/bookings/stats?days=${days}`;
 
 	try {
-		const response = await fetch(url);
+		const response = await axios(url);
 
-		if (!response.ok) {
-			throw new Error(`HTTP error! Status: ${response.status}`);
-		}
-
-		return await response.json();
-	} catch (err) {
-		console.log(err);
+		return await response;
+	} catch (err: any) {
+		throw new Error(`HTTP error! Status: ${err.status}`);
 	}
 }
 export async function fetchStaysAfterDate(days: number) {
 	const url = `/api/stays?days=${days}`;
 
 	try {
-		const response = await fetch(url);
-
-		if (!response.ok) {
-			throw new Error(`HTTP error! Status: ${response.status}`);
-		}
-
-		return await response.json();
-	} catch (err) {
-		console.log(err);
+		const response = await axios(url);
+		return await response;
+	} catch (err: any) {
+		throw new Error(`HTTP error! Status: ${err.status}`);
 	}
 }
 
@@ -35,60 +26,34 @@ export async function fetchDailyStats() {
 	const url = `/api/bookings/daily`;
 
 	try {
-		const response = await fetch(url);
-
-		if (!response.ok) {
-			throw new Error(`HTTP error! Status: ${response.status}`);
-		}
-
-		return await response.json();
-	} catch (err) {
-		console.log(err);
+		const response = await axios(url);
+		return await response;
+	} catch (err: any) {
+		throw new Error(`HTTP error! Status: ${err.status}`);
 	}
 }
 
 export async function handleDeleteBookings(id: any) {
 	try {
-		const res = await fetch(
-			`/api/bookings/${id}`,
-
-			{
-				method: 'DELETE' // *GET, POST, PUT, DELETE, etc.
-				// body data type must match "Content-Type" header
-			}
-		);
-
-		if (!res.ok) {
-			throw new Error(`Cabin could not be created Status: ${res.status}`);
-		}
-
-		return res.json(); // parses JSON response into native JavaScript objects
-	} catch (err) {
-		console.log(err);
+		const res = await axios.delete(`/api/bookings/${id}`);
+		return await res;
+	} catch (err: any) {
+		throw new Error(`Cabin could not be created Status: ${err.status}`);
 	}
 }
 
 export async function handleCheckout(payload: any, id: any) {
 	try {
-		const res = await fetch(`/api/bookings/${id}`, {
-			method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
-			body: JSON.stringify(payload) // body data type must match "Content-Type" header
-		});
-
-		if (!res.ok) {
-			throw new Error(
-				`Guest could not be checked out Status: ${res.status}`
-			);
-		}
-
-		return await res.json(); // parses JSON response into native JavaScript objects
-	} catch (err) {
-		console.log(err);
+		const res = await axios.patch(`/api/bookings/${id}`, payload);
+		return await res;
+	} catch (err: any) {
+		throw new Error(`Guest could not be checked out Status: ${err.status}`);
 	}
 }
 export async function handleCheckIn(payload: any, id: any) {
 	try {
 		const res = await axios.patch(`/api/bookings/${id}`, payload);
+		return await res;
 	} catch (err: any) {
 		throw new Error(`Guest could not be checked out Status: ${err.status}`);
 	}
