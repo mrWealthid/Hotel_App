4e0fa601c3e0f9e522ca9316f13de9c5a844e648
wealthiduwe <wealthiduwe@gmail.com>
2023-11-16 13:03:52 -0500

    Update: Updated verbage for route

--- src/components/ui/SideBarComponent.tsx
+++ src/components/ui/SideBarComponent.tsx
@@ -10,7 +10,7 @@ export interface IRoute {
 }
 const SideBar = () => {
 	const routes: IRoute[] = [
-		{ name: 'Home', path: '/dashboard' },
+		{ name: 'Overview', path: '/dashboard' },
 		{ name: 'Bookings', path: '/dashboard/bookings' },
 		{ name: 'Cabins', path: '/dashboard/cabins' },
 		{ name: 'Users', path: '/dashboard/users' },
@@ -55,7 +55,7 @@ const SideBar = () => {
 				} sm:translate-x-0`}>
 				<div className="flex flex-col  min-h-screen  bg-primary dark:glass text-white py-4">
 					<p className="flex justify-center space-x-4 text-white">
-						Logo
+						BookStays
 					</p>
 					<section className="flex flex-col  gap-3  mt-10 px-4">
 						{routes.map((link) => {
