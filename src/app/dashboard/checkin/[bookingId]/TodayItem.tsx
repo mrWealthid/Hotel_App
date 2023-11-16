b5ca1af82c60914776e8f5b0acc0383082ae0a68
wealthiduwe <wealthiduwe@gmail.com>
2023-11-16 13:06:16 -0500

    Add: Added a confirmation modal for checkout from the Overview page

--- src/app/dashboard/checkin/[bookingId]/TodayItem.tsx
+++ src/app/dashboard/checkin/[bookingId]/TodayItem.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import React from 'react';
 import { useCheckOutBooking } from '../../hooks/useDashboard';
+import Modal from '@/components/shared/Modal/Modal-component';
+import ConfirmationPage from '@/components/ui/ConfirmationPage';
 
 const TodayItem = ({ checkStatus, guests, _id, numNights }: any) => {
 	const { isCheckingOut, checkOutBooking } = useCheckOutBooking(_id);
@@ -30,14 +32,26 @@ const TodayItem = ({ checkStatus, guests, _id, numNights }: any) => {
 			)}
 			{checkStatus === 'CHECKED_IN' && (
 				<div>
-					<button
-						type="button"
-						onClick={() =>
-							checkOutBooking({ checkStatus: 'CHECKED_OUT' })
-						}
-						className=" bg-primary disabled:bg-primary-light disabled:cursor-not-allowed text-white px-4 py-2 rounded-3xl">
-						Check-Out
-					</button>
+					<Modal>
+						<Modal.Open opens="check-out">
+							<button className=" bg-primary disabled:bg-primary-light disabled:cursor-not-allowed text-white px-4 py-2 rounded-3xl">
+								Check-Out
+							</button>
+						</Modal.Open>
+
+						<Modal.Window name="check-out">
+							<ConfirmationPage
+								handler={(onCloseModal: any) => {
+									checkOutBooking({
+										checkStatus: 'CHECKED_OUT'
+									});
+									onCloseModal();
+								}}
+								modalText={`Are you sure you want to checkout
+							 ${guests.name}`}
+							/>
+						</Modal.Window>
+					</Modal>
 				</div>
 			)}
 		</div>
