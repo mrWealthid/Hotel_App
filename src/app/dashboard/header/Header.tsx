8db23465873f6d699d19fee7ab88838f66e36232
wealthiduwe <wealthiduwe@gmail.com>
2023-11-19 19:45:08 -0500

    add: Added sticky to the header for scroll

--- src/app/dashboard/header/Header.tsx
+++ src/app/dashboard/header/Header.tsx
@@ -8,7 +8,7 @@ import Logout from './Logout';
 
 const Header = async () => {
 	return (
-		<div className="py-4 px-4  text-sm glass items-center   w-full flex justify-end gap-3 text-black dark:text-white">
+		<div className="py-4 px-4  text-sm glass items-center sticky top-0   w-full flex justify-end gap-3 text-black dark:text-white">
 			<Profile />
 			<SwitchToggle />
 			<Link href={'/dashboard/account'}>Account</Link>
