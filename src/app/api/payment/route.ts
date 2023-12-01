f50085aa6e6e5bd07db899a4fd4747649474dd05
wealthiduwe <wealthiduwe@gmail.com>
2023-11-30 20:50:21 -0500

    fixes

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -10,46 +10,33 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 const secret = process.env.STRIPE_WEBHOOK_SECRET || '';
 export async function POST(request: NextRequest) {
-	let event: Stripe.Event;
-
 	try {
 		const body = await request.text();
 		const signature = headers().get('stripe-signature')!;
-		event = stripe.webhooks.constructEvent(body, signature, secret);
+
+		console.log('Signature==>', signature);
+
+		const event = stripe.webhooks.constructEvent(body, signature, secret);
+
+		if (event.type === 'checkout.session.completed') {
+			handlePaymentSessionCompleted(event.data.object);
+		}
+
+		return NextResponse.json({ result: event, ok: true });
 	} catch (err: any) {
 		return NextResponse.json(
 			{ error: `Webhook Error: ${err.message}` },
 			{ status: 400 }
 		);
 	}
-	// Handle the event
-	switch (event.type) {
-		case 'checkout.session.completed':
-			const checkoutSessionCompleted = event.data.object;
-
-			const metadata = event.data.object.metadata;
-
-			if (metadata && metadata.source === 'Payment') {
-				handlePaymentSessionCompleted(checkoutSessionCompleted);
-			}
-			break;
-
-		default:
-			console.log(`Unhandled event type ${event.type}`);
-	}
 
 	// Return a 200 response to acknowledge receipt of the event
-	return NextResponse.json(
-		{
-			status: 'success',
-			mesage: 'Recieved'
-		},
-		{ status: 200 }
-	);
 }
 
 async function handlePaymentSessionCompleted(session: any) {
 	const bookingId = session.client_reference_id;
+
+	console.log('Id===>', bookingId);
 
 	await Booking.findByIdAndUpdate(bookingId, {
 		isPaid: true
