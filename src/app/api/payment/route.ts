c94c12283c891498016278130798c13bb68b6577
wealthiduwe <wealthiduwe@gmail.com>
2023-11-30 20:41:14 -0500

    fix

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,25 +1,21 @@
-import BookingForm from '@/app/dashboard/bookings/BookingForm';
 import { connect } from '@/dbConfig/dbConfig';
 import Booking from '@/model/bookingsModel';
-import { NextApiRequest } from 'next';
+
 import { headers } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 connect();
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+const secret = process.env.STRIPE_WEBHOOK_SECRET || '';
 export async function POST(request: NextRequest) {
 	let event: Stripe.Event;
 
-	const body = await request.text();
-	const signature = headers().get('stripe-signature')!;
-
 	try {
-		event = stripe.webhooks.constructEvent(
-			body,
-			signature,
-			process.env.STRIPE_WEBHOOK_SECRET!
-		);
+		const body = await request.text();
+		const signature = headers().get('stripe-signature')!;
+		event = stripe.webhooks.constructEvent(body, signature, secret);
 	} catch (err: any) {
 		return NextResponse.json(
 			{ error: `Webhook Error: ${err.message}` },
@@ -50,71 +46,12 @@ export async function POST(request: NextRequest) {
 		},
 		{ status: 200 }
 	);
-
-	// try {
-	// 	//2) Check if user exists & password is correct after it's hashed
-	// 	const session = await stripe.checkout.sessions.create({
-	// 		payment_method_types: ['card'],
-	// 		success_url: process.env.STRIPE_SUCCESS_URL,
-	// 		cancel_url: process.env.STRIPE_FAILURE_URL,
-	// 		customer_email: req.user.email,
-	// 		client_reference_id: beneficiary.id,
-	// 		mode: 'payment',
-	// 		line_items: [
-	// 			{
-	// 				price_data: {
-	// 					currency: 'usd',
-	// 					unit_amount: amount * 100,
-	// 					product_data: {
-	// 						name: `${beneficiary.name}`,
-	// 						description: beneficiary.accountNumber,
-	// 						images: [beneficiary.imgUrl]
-	// 					}
-	// 				},
-	// 				quantity: 1
-	// 			}
-	// 		],
-	// 		metadata: {
-	// 			source: 'Payment'
-	// 		}
-	// 	});
-	// 	const response = NextResponse.json({
-	// 		status: 'success',
-	// 		session
-	// 		// data: settings[0]
-	// 	});
-	// 	return response;
-	// } catch (error: any) {
-	// 	return NextResponse.json({ error: error.message }, { status: 500 });
-	// }
 }
 
 async function handlePaymentSessionCompleted(session: any) {
 	const bookingId = session.client_reference_id;
 
-	// const beneficiary = await User.findById(beneficiaryId);
-	// const userDetails = await User.find({ email: email });
-
-	// const initiator = userDetails.initiatorAccountNumber;
-	// const beneficiary = beneficiaryDetails.beneficiaryAccountNumber;
-
-	// if (initiator === beneficiary)
-	//   return next(new AppError("You can't Transfer to self", 404));
-
-	// const payload = {
-	// 	initiatorName: userDetails[0].name,
-	// 	beneficiaryAccountNumber: beneficiary.accountNumber,
-	// 	initiatorAccountNumber: userDetails[0].accountNumber,
-	// 	amount: session.amount_total / 100,
-	// 	transactionType: 'Credit',
-	// 	user: beneficiary.id,
-	// 	createdAt: new Date(Date.now())
-	// };
-
 	await Booking.findByIdAndUpdate(bookingId, {
 		isPaid: true
 	});
-
-	//settlement
-	// await Transaction.create({...payload, amount: (session.amount_total/100) * -1, transactionType:'Debit',  user: userDetails[0].id});
 }
