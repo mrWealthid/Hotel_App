f16a65c5d905d030b9343777162963c0eb3e2c86
wealthiduwe <wealthiduwe@gmail.com>
2023-12-08 11:58:52 -0500

    Update: Updated checkout flow for receipt print out from dashboard

--- src/app/dashboard/checkin/[bookingId]/TodayActivity.tsx
+++ src/app/dashboard/checkin/[bookingId]/TodayActivity.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodayItem from './TodayItem';
+import ReceiptPopup from '@/components/shared/Modal/ReceiptPopup';
 
 const TodayActivity = ({ daily }: any) => {
-	if (daily?.length < 1) {
-		return (
-			<p className="  text-xs   dark:glass p-3 dark:rounded   items-center  ">
-				No Data Available
-			</p>
-		);
+	const [show, setShow] = useState(false);
+	const [activity, setActivity] = useState(null);
+
+	function handlePopup() {
+		setShow(true);
 	}
+
+	function renderBookings() {
+		if (daily?.length < 1) {
+			return (
+				<p className="  text-xs   dark:glass p-3 dark:rounded   items-center  ">
+					No Data Available
+				</p>
+			);
+		}
+		if (daily?.length >= 1) {
+			return daily?.map((activity: any) => (
+				<TodayItem
+					handlePopup={handlePopup}
+					processActivity={setActivity}
+					key={activity._id}
+					activity={activity}
+					{...activity}
+				/>
+			));
+		}
+	}
+
 	return (
 		<div className=" flex flex-col gap-2">
-			{daily?.map((activity: any) => (
-				<TodayItem key={activity._id} {...activity} />
-			))}
+			{renderBookings()}
+
+			{show && <ReceiptPopup activity={activity} />}
 		</div>
 	);
 };
--- src/app/dashboard/checkin/[bookingId]/TodayItem.tsx
+++ src/app/dashboard/checkin/[bookingId]/TodayItem.tsx
@@ -4,60 +4,84 @@ import { useCheckOutBooking } from '../../hooks/useDashboard';
 import Modal from '@/components/shared/Modal/Modal-component';
 import ConfirmationPage from '@/components/ui/ConfirmationPage';
 
-const TodayItem = ({ checkStatus, guests, _id, numNights }: any) => {
+const TodayItem = ({
+	activity,
+	checkStatus,
+	guests,
+	_id,
+	numNights,
+	handlePopup,
+	processActivity
+}: any) => {
 	const { isCheckingOut, checkOutBooking } = useCheckOutBooking(_id);
+
+	function handleCheckout(onCloseModal: any) {
+		checkOutBooking(
+			{
+				checkStatus: 'CHECKED_OUT'
+			},
+			{
+				onSuccess: function () {
+					onCloseModal();
+					handlePopup(true);
+					processActivity(activity);
+				}
+			}
+		);
+	}
 	return (
-		<div className="flex  text-xs justify-between  border-b dark:border-none dark:glass dark:p-2 dark:rounded   items-center pb-2 border-gray-50 ">
-			<p
-				className={`${
-					checkStatus === 'CHECKED_IN'
-						? 'bg-green-300 text-green-800'
-						: 'bg-blue-400 text-white'
-				}  rounded-3xl  py-1 px-2`}>
-				{checkStatus === 'CHECKED_IN' ? 'Departing' : 'Arriving'}
-			</p>
+		<section>
+			<div className="flex  text-xs justify-between  border-b dark:border-none dark:glass dark:p-2 dark:rounded   items-center pb-2 border-gray-50 ">
+				<p
+					className={`${
+						checkStatus === 'CHECKED_IN'
+							? 'bg-green-300 text-green-800'
+							: 'bg-blue-400 text-white'
+					}  rounded-3xl  py-1 px-2`}>
+					{checkStatus === 'CHECKED_IN' ? 'Departing' : 'Arriving'}
+				</p>
 
-			<div>
-				<p>{guests?.name}</p>
-				<small>{guests?.email}</small>
-			</div>
-
-			<p>{numNights} night(s)</p>
-
-			{checkStatus === 'UNCONFIRMED' && (
 				<div>
-					<button
-						type="button"
-						className=" bg-primary disabled:bg-primary-light disabled:cursor-not-allowed text-white px-4 py-2 rounded-3xl">
-						<Link href={`/dashboard/checkin/${_id}`}>Check-In</Link>
-					</button>
+					<p>{guests?.name}</p>
+					<small>{guests?.email}</small>
 				</div>
-			)}
-			{checkStatus === 'CHECKED_IN' && (
-				<div>
-					<Modal>
-						<Modal.Open opens="check-out">
-							<button className=" bg-primary disabled:bg-primary-light disabled:cursor-not-allowed text-white px-4 py-2 rounded-3xl">
-								Check-Out
-							</button>
-						</Modal.Open>
 
-						<Modal.Window name="check-out">
-							<ConfirmationPage
-								handler={(onCloseModal: any) => {
-									checkOutBooking({
-										checkStatus: 'CHECKED_OUT'
-									});
-									onCloseModal();
-								}}
-								modalText={`Are you sure you want to checkout
+				<p>{numNights} night(s)</p>
+
+				{checkStatus === 'UNCONFIRMED' && (
+					<div>
+						<button
+							type="button"
+							className=" bg-primary disabled:bg-primary-light disabled:cursor-not-allowed text-white px-4 py-2 rounded-3xl">
+							<Link href={`/dashboard/checkin/${_id}`}>
+								Check-In
+							</Link>
+						</button>
+					</div>
+				)}
+				{checkStatus === 'CHECKED_IN' && (
+					<div>
+						<Modal>
+							<Modal.Open opens="check-out">
+								<button className=" bg-primary disabled:bg-primary-light disabled:cursor-not-allowed text-white px-4 py-2 rounded-3xl">
+									Check-Out
+								</button>
+							</Modal.Open>
+
+							<Modal.Window name="check-out">
+								<ConfirmationPage
+									handler={(onCloseModal: any) => {
+										handleCheckout(onCloseModal);
+									}}
+									modalText={`Are you sure you want to checkout
 							 ${guests.name}`}
-							/>
-						</Modal.Window>
-					</Modal>
-				</div>
-			)}
-		</div>
+								/>
+							</Modal.Window>
+						</Modal>
+					</div>
+				)}
+			</div>
+		</section>
 	);
 };
 
