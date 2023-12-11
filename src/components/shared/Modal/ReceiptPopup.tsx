7ecf98d4a5dbc34610d9b1b4403c73cfa21a4fd0
wealthiduwe <wealthiduwe@gmail.com>
2023-12-11 18:32:04 -0500

    Add: Added Receipt Reusable receipt popup

--- src/components/shared/Modal/ReceiptPopup.tsx
+++ src/components/shared/Modal/ReceiptPopup.tsx
@@ -1,9 +1,16 @@
 import { Fragment, useRef, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { ExclamationCircleIcon } from '@heroicons/react/outline';
+import ReceiptPage from '@/components/ui/ReceiptPage';
+import { useReactToPrint } from 'react-to-print';
 
 export default function ReceiptPopup({ activity }: any) {
 	const [open, setOpen] = useState(true);
+
+	const componentRef = useRef(null);
+	const handlePrint = useReactToPrint({
+		content: () => componentRef.current
+	});
 
 	const cancelButtonRef = useRef(null);
 
@@ -11,7 +18,7 @@ export default function ReceiptPopup({ activity }: any) {
 		<Transition.Root show={open} as={Fragment}>
 			<Dialog
 				as="div"
-				className="relative z-10"
+				className="relative z-10 reciept-modal"
 				initialFocus={cancelButtonRef}
 				onClose={setOpen}>
 				<Transition.Child
@@ -35,28 +42,27 @@ export default function ReceiptPopup({ activity }: any) {
 							leave="ease-in duration-200"
 							leaveFrom="opacity-100 translate-y-0 sm:scale-100"
 							leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-							<Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-								<div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-									<div className="sm:flex sm:items-start">
-										<div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+							<Dialog.Panel className="receipt-content relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+								<div className="bg-white ">
+									<div className="">
+										{/* <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
 											<ExclamationCircleIcon
 												className="h-6 w-6 text-red-600"
 												aria-hidden="true"
 											/>
-										</div>
-										<div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+										</div> */}
+
+										{/* <p ref={componentRef}>Wealth Iduwe</p> */}
+										<div className="">
 											<Dialog.Title
 												as="h3"
-												className="text-base font-semibold leading-6 text-gray-900">
-												Deactivate account {activity.id}
-											</Dialog.Title>
-											<div className="mt-2">
+												className="text-base font-semibold leading-6 text-gray-900"></Dialog.Title>
+											<div className="">
 												<p className="text-sm text-gray-500">
-													Are you sure you want to
-													deactivate your account? All
-													of your data will be
-													permanently removed. This
-													action cannot be undone.
+													<ReceiptPage
+														ref={componentRef}
+														{...activity}
+													/>
 												</p>
 											</div>
 										</div>
@@ -67,7 +73,16 @@ export default function ReceiptPopup({ activity }: any) {
 										type="button"
 										className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
 										onClick={() => setOpen(false)}>
-										Deactivate
+										Email Reciept
+									</button>
+									<button
+										type="button"
+										className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+										onClick={() => {
+											setOpen(false);
+											handlePrint();
+										}}>
+										Print Receipt
 									</button>
 									<button
 										type="button"
