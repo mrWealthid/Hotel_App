4fd2c8d5677d3162dcf44d5dbec39feb9ab79cad
wealthiduwe <wealthiduwe@gmail.com>
2023-12-17 22:20:02 -0500

    Add: Added static pages for password update

--- src/app/auth/forgotPassword/page.tsx
+++
@@ -1,8 +0,0 @@
-import ReceiptPage from '@/components/ui/ReceiptPage';
-import React from 'react';
-
-const forgotPasswordComponent = () => {
-	return <ReceiptPage />;
-};
-
-export default forgotPasswordComponent;
---
+++ src/app/auth/updatePassword/[resetToken]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UpdatePasswordComponent = () => {
+	return <p>Update Password page</p>;
+};
+
+export default UpdatePasswordComponent;
