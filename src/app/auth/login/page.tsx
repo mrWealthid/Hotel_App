ddfe4be9fc29fb822c79788b0e828d584cc55e0a
wealthiduwe <wealth.iduwe@vendease.com>
2025-05-30 23:59:31 -0400

    Refactor login and signup components for improved code consistency and readability

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,48 +1,48 @@
-'use client';
+"use client";
 
-import EmailInput from '@/components/shared/Form-inputs/Email-Input';
-import TextInput from '@/components/shared/Form-inputs/Text-Input';
-import ButtonComponent from '@/components/shared/Form-inputs/Button';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
-import { useLogin } from '../hooks/useAuth';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import EmailInput from "@/components/shared/Form-inputs/Email-Input";
+import TextInput from "@/components/shared/Form-inputs/Text-Input";
+import ButtonComponent from "@/components/shared/Form-inputs/Button";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useLogin } from "../hooks/useAuth";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 // import axiosInstance from '@/app/utils/intercerptor';
 
 const LoginComponent = () => {
-	const { register, handleSubmit, getValues, formState } = useForm({
-		mode: 'onChange',
-		defaultValues: { email: 'admin@gmail.com', password: '12345678' }
-	});
+  const { register, handleSubmit, getValues, formState } = useForm({
+    mode: "onChange",
+    defaultValues: { email: "admin@gmail.com", password: "12345678" },
+  });
 
-	const router = useRouter();
-	const { isLoading, login } = useLogin();
+  const router = useRouter();
+  const { isLoading, login } = useLogin();
 
-	async function onSubmit(payload: any) {
-		login(payload, { onSuccess: () => router.push('/dashboard') });
-	}
+  async function onSubmit(payload: any) {
+    login(payload, { onSuccess: () => router.push("/dashboard") });
+  }
 
-	const { errors, isSubmitting } = formState;
+  const { errors, isSubmitting } = formState;
 
-	function onError(err: any) {
-		console.log(err);
-	}
-	const [showPassword, setShowPassword] = useState(false);
+  function onError(err: any) {
+    console.log(err);
+  }
+  const [showPassword, setShowPassword] = useState(false);
 
-	const togglePassword = () => {
-		setShowPassword(!showPassword);
-	};
-	return (
-		<>
-			<section className='flex flex-col min-h-screen h-fit items-center justify-center'>
-				<section className='bg-white dark:glass w-5/6 md:w-4/6 lg:w-1/3 py-10 px-5 flex gap-4 flex-col items-center justify-center'>
-					<p className='text-center text-primary dark:text-label-color font-bold text-2xl'>
-						Sign In to Get Started
-					</p>
-					{/*
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+  return (
+    <>
+      <section className="flex flex-col min-h-screen h-fit items-center justify-center">
+        <section className="bg-white dark:glass w-5/6 md:w-4/6 lg:w-1/3  p-10 flex gap-4 flex-col items-center justify-center">
+          <p className="text-center text-primary dark:text-label-color font-bold text-2xl">
+            Sign In to Get Started
+          </p>
+          {/*
 					<section className="flex flex-col gap-3 items-center justify-center w-full">
 						<button className="btn flex gap-3 btn-primary !w-5/6">
 							Google
@@ -53,93 +53,89 @@ const LoginComponent = () => {
 						</section>
 					</section> */}
 
-					<section className='w-full'>
-						<form
-							onSubmit={handleSubmit(onSubmit, onError)}
-							action=''
-							className='w-full flex flex-col justify-center gap-2 items-center'>
-							<EmailInput
-								name={'email'}
-								label='Email'
-								error={errors?.['email']?.message?.toString()}>
-								<input
-									{...register('email', {
-										required: 'This field is required',
-										pattern: {
-											value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-											message: 'Invalid email address'
-										}
-									})}
-									className='input-style'
-									type='email'
-									id='name'
-								/>
-							</EmailInput>
-							<TextInput
-								name={'password'}
-								placeholder='Enter Password'
-								label='Password'
-								error={errors?.[
-									'password'
-								]?.message?.toString()}>
-								<div className='input-style !p-0 !pr-2 !overflow-hidden'>
-									<input
-										className='w-full  dark:bg-transparent   border-none outline-none focus:ring-0 ring-0 '
-										type={
-											showPassword ? 'text' : 'password'
-										}
-										{...register('password', {
-											required: 'This field is required'
-										})}
-										id='psw'
-									/>
+          <section className="w-full">
+            <form
+              onSubmit={handleSubmit(onSubmit, onError)}
+              className="w-full flex flex-col justify-center gap-2 items-center"
+            >
+              <EmailInput
+                name={"email"}
+                label="Email"
+                error={errors?.["email"]?.message?.toString()}
+              >
+                <input
+                  {...register("email", {
+                    required: "This field is required",
+                    pattern: {
+                      value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                      message: "Invalid email address",
+                    },
+                  })}
+                  className="input-style"
+                  type="email"
+                  id="name"
+                />
+              </EmailInput>
+              <TextInput
+                name={"password"}
+                placeholder="Enter Password"
+                label="Password"
+                error={errors?.["password"]?.message?.toString()}
+              >
+                <div className="input-style !p-0 !pr-2 !overflow-hidden">
+                  <input
+                    className="w-full  dark:bg-transparent   border-none outline-none focus:ring-0 ring-0 "
+                    type={showPassword ? "text" : "password"}
+                    {...register("password", {
+                      required: "This field is required",
+                    })}
+                    id="psw"
+                  />
 
-									{!showPassword ? (
-										<FaEyeSlash
-											className='text-primary cursor-pointer'
-											onClick={togglePassword}
-										/>
-									) : (
-										<FaEye
-											className='text-primary cursor-pointer'
-											onClick={togglePassword}
-										/>
-									)}
-								</div>
-							</TextInput>
+                  {!showPassword ? (
+                    <FaEyeSlash
+                      className="text-primary cursor-pointer"
+                      onClick={togglePassword}
+                    />
+                  ) : (
+                    <FaEye
+                      className="text-primary cursor-pointer"
+                      onClick={togglePassword}
+                    />
+                  )}
+                </div>
+              </TextInput>
 
-							<section className=' '>
-								<ButtonComponent
-									styles='rounded-3xl'
-									btnText='Submit'
-
-									loading={isLoading}
-									type='submit'
-									disabled={!formState.isValid || isLoading}
-								/>
-							</section>
-							<p className='flex gap-3 text-sm text-primary dark:text-label-color'>
-								Forgot Password ?
-								<Link
-									href={'/auth/resetPassword'}
-									className='text-blue-600 text-sm'>
-									Reset
-								</Link>
-							</p>
-							<p className='flex gap-3 text-sm text-primary dark:text-label-color'>
-								Need An Account ?
-								<Link
-									href={'/auth/signup'}
-									className='text-blue-600 text-sm'>
-									Sign up
-								</Link>
-							</p>
-						</form>
-					</section>
-				</section>
-			</section>
-		</>
-	);
+              <section className=" ">
+                <ButtonComponent
+                  styles="rounded-3xl"
+                  btnText="Submit"
+                  loading={isLoading}
+                  type="submit"
+                  disabled={!formState.isValid || isLoading}
+                />
+              </section>
+              <p className="flex gap-3 text-sm text-primary dark:text-label-color">
+                Forgot Password ?
+                <Link
+                  href={"/auth/resetPassword"}
+                  className="text-blue-600 text-sm"
+                >
+                  Reset
+                </Link>
+              </p>
+              <p className="flex gap-3 text-sm text-primary dark:text-label-color">
+                Need An Account ?
+                <Link href={"/auth/signup"} className="text-blue-600 text-sm">
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </section>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default LoginComponent;
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,53 +1,53 @@
-'use client';
+"use client";
 // import axiosInstance from '@/app/utils/intercerptor';
-import EmailInput from '@/components/shared/Form-inputs/Email-Input';
-import TextInput from '@/components/shared/Form-inputs/Text-Input';
-import ButtonComponent from '@/components/shared/Form-inputs/Button';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useRegister } from '../hooks/useAuth';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import Link from 'next/link';
+import EmailInput from "@/components/shared/Form-inputs/Email-Input";
+import TextInput from "@/components/shared/Form-inputs/Text-Input";
+import ButtonComponent from "@/components/shared/Form-inputs/Button";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRegister } from "../hooks/useAuth";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import Link from "next/link";
 
 const SignupComponent = () => {
-	const { register, handleSubmit, getValues, formState } = useForm({
-		mode: 'onChange'
-	});
+  const { register, handleSubmit, getValues, formState } = useForm({
+    mode: "onChange",
+  });
 
-	const router = useRouter();
+  const router = useRouter();
 
-	const { isLoading, registering } = useRegister();
-	const { errors, isSubmitting } = formState;
+  const { isLoading, registering } = useRegister();
+  const { errors, isSubmitting } = formState;
 
-	async function onSubmit(data: any) {
-		const { firstName, lastName, ...rest } = data;
+  async function onSubmit(data: any) {
+    const { firstName, lastName, ...rest } = data;
 
-		const payload = { name: firstName + ' ' + lastName, ...rest };
+    const payload = { name: firstName + " " + lastName, ...rest };
 
-		registering(payload, { onSuccess: () => router.push('/dashboard') });
-	}
+    registering(payload, { onSuccess: () => router.push("/dashboard") });
+  }
 
-	function onError(err: any) {
-		console.log(err);
-	}
+  function onError(err: any) {
+    console.log(err);
+  }
 
-	const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-	const togglePassword = () => {
-		setShowPassword(!showPassword);
-	};
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-	return (
-		<>
-			<section className='flex flex-col min-h-screen items-center justify-center'>
-				<section className='bg-white dark:glass lg:my-5 w-5/6 md:w-4/6 lg:w-1/2 py-10 px-6 flex gap-6 flex-col items-center justify-center'>
-					<p className='text-center text-primary dark:text-label-color font-bold text-2xl'>
-						Sign Up
-					</p>
+  return (
+    <>
+      <section className="flex flex-col min-h-screen items-center justify-center">
+        <section className="bg-white dark:glass lg:my-5 w-5/6 md:w-4/6 lg:w-1/2  p-10 flex gap-6 flex-col items-center justify-center">
+          <p className="text-center text-primary dark:text-label-color font-bold text-2xl">
+            Sign Up
+          </p>
 
-					{/* <section className="flex flex-col gap-3 items-center justify-center w-full">
+          {/* <section className="flex flex-col gap-3 items-center justify-center w-full">
 						<button className="btn flex gap-3 btn-primary">
 							Google
 						</button>
@@ -56,122 +56,117 @@ const SignupComponent = () => {
 							Or
 						</section>
 					</section> */}
-					<form
-						onSubmit={handleSubmit(onSubmit, onError)}
-						className='w-full flex flex-col gap-2 items-center'>
-						<div className='w-full flex gap-4'>
-							<TextInput
-								name={'firstName'}
-								label='First Name'
-								type='password'
-								error={errors?.[
-									'firstName'
-								]?.message?.toString()}>
-								<input
-									{...register('firstName', {
-										required: 'This field is required'
-									})}
-									className='input-style'
-									type='text'
-									id='firstName'
-									placeholder='Enter First Name'
-								/>
-							</TextInput>
-							<TextInput
-								name={'lastName'}
-								label='Last Name'
-								error={errors?.[
-									'lastName'
-								]?.message?.toString()}>
-								<input
-									{...register('lastName', {
-										required: 'This field is required'
-									})}
-									className='input-style'
-									type='text'
-									id='lastName'
-									placeholder='Enter Last Name'
-								/>
-							</TextInput>
-						</div>
-						<div className='w-full flex gap-4'>
-							<EmailInput
-								name={'email'}
-								label='Email'
-								error={errors?.['email']?.message?.toString()}>
-								<input
-									{...register('email', {
-										required: 'This field is required',
-										pattern: {
-											value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-											message: 'Invalid email address'
-										}
-									})}
-									className='input-style  '
-									type='email'
-									id='email'
-									placeholder='johndoe@gmail.com'
-								/>
-							</EmailInput>
+          <form
+            onSubmit={handleSubmit(onSubmit, onError)}
+            className="w-full flex flex-col gap-2 items-center"
+          >
+            <div className="w-full flex gap-4">
+              <TextInput
+                name={"firstName"}
+                label="First Name"
+                type="password"
+                error={errors?.["firstName"]?.message?.toString()}
+              >
+                <input
+                  {...register("firstName", {
+                    required: "This field is required",
+                  })}
+                  className="input-style"
+                  type="text"
+                  id="firstName"
+                  placeholder="Enter First Name"
+                />
+              </TextInput>
+              <TextInput
+                name={"lastName"}
+                label="Last Name"
+                error={errors?.["lastName"]?.message?.toString()}
+              >
+                <input
+                  {...register("lastName", {
+                    required: "This field is required",
+                  })}
+                  className="input-style"
+                  type="text"
+                  id="lastName"
+                  placeholder="Enter Last Name"
+                />
+              </TextInput>
+            </div>
+            <div className="w-full flex gap-4">
+              <EmailInput
+                name={"email"}
+                label="Email"
+                error={errors?.["email"]?.message?.toString()}
+              >
+                <input
+                  {...register("email", {
+                    required: "This field is required",
+                    pattern: {
+                      value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                      message: "Invalid email address",
+                    },
+                  })}
+                  className="input-style  "
+                  type="email"
+                  id="email"
+                  placeholder="johndoe@gmail.com"
+                />
+              </EmailInput>
 
-							<TextInput
-								name={'password'}
-								placeholder='Enter Password'
-								label='Password'
-								error={errors?.[
-									'password'
-								]?.message?.toString()}>
-								<div className='input-style !p-0 !pr-2 !overflow-hidden'>
-									<input
-										placeholder='Enter Password'
-										className='w-full  dark:bg-transparent  border-none outline-none focus:ring-0 ring-0 '
-										type={
-											showPassword ? 'text' : 'password'
-										}
-										{...register('password', {
-											required: 'This field is required'
-										})}
-										id='psw'
-									/>
+              <TextInput
+                name={"password"}
+                placeholder="Enter Password"
+                label="Password"
+                error={errors?.["password"]?.message?.toString()}
+              >
+                <div className="input-style !p-0 !pr-2 !overflow-hidden">
+                  <input
+                    placeholder="Enter Password"
+                    className="w-full  dark:bg-transparent  border-none outline-none focus:ring-0 ring-0 "
+                    type={showPassword ? "text" : "password"}
+                    {...register("password", {
+                      required: "This field is required",
+                    })}
+                    id="psw"
+                  />
 
-									{!showPassword ? (
-										<FaEyeSlash
-											className='text-primary cursor-pointer'
-											onClick={togglePassword}
-										/>
-									) : (
-										<FaEye
-											className='text-primary cursor-pointer'
-											onClick={togglePassword}
-										/>
-									)}
-								</div>
-							</TextInput>
-						</div>
+                  {!showPassword ? (
+                    <FaEyeSlash
+                      className="text-primary cursor-pointer"
+                      onClick={togglePassword}
+                    />
+                  ) : (
+                    <FaEye
+                      className="text-primary cursor-pointer"
+                      onClick={togglePassword}
+                    />
+                  )}
+                </div>
+              </TextInput>
+            </div>
 
-						<section className=' '>
-							<ButtonComponent
-								styles="rounded-3xl"
-								btnText="Submit"
-								type="submit"
-								loading={isLoading}
-								disabled={!formState.isValid || isLoading}
-								// afterIcon="/assets/send.svg"
-							/>
-						</section>
-						<p className='text-sm text-primary dark:text-label-color flex gap-2'>
-							Already Have An Account ?
-							<Link
-								href={'/auth/login'}
-								className='text-blue-600 text-sm'>
-								Login
-							</Link>
-						</p>
-					</form>
-				</section>
-			</section>
-		</>
-	);
+            <section className=" ">
+              <ButtonComponent
+                styles="rounded-3xl"
+                btnText="Submit"
+                type="submit"
+                loading={isLoading}
+                disabled={!formState.isValid || isLoading}
+                // afterIcon="/assets/send.svg"
+              />
+            </section>
+            <p className="text-sm text-primary dark:text-label-color flex gap-2">
+              Already Have An Account ?
+              <Link href={"/auth/login"} className="text-blue-600 text-sm">
+                Login
+              </Link>
+            </p>
+          </form>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default SignupComponent;
