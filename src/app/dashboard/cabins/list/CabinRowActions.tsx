d765660554f006afac03949e57343757072c6158
Wealth Iduwe <wealthiduwe@gmail.com>
2025-06-03 13:00:49 -0400

    refactor: Reorganize import statements in CabinRowActions for improved readability

--- src/app/dashboard/cabins/list/CabinRowActions.tsx
+++ src/app/dashboard/cabins/list/CabinRowActions.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React, { Fragment } from "react";
-import Modal from "@/components/shared/modal/Modal";
+
 import CabinForm from "../CabinForm";
-import ConfirmationPage from "@/components/ui/ConfirmationPage";
 import { Menu, Transition } from "@headlessui/react";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useDeleteCabin, useDuplicateCabin } from "../hooks/useCabins";
 import { CgMenuGridO } from "react-icons/cg";
+import ConfirmationPage from "@/components/ui/ConfirmationPage";
+import Modal from "@/components/shared/modal/Modal";
 
 const CabinRowActions = ({ rowData }: any) => {
   const { isDeleting, deleteCabin } = useDeleteCabin();
